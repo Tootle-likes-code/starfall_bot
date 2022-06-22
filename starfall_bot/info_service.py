c5c5ad388c2f9ff09d570293456dadfe7984793a
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-06-22 22:27:53 +0100

    Added pylint

--- starfall_bot/info_service.py
+++ starfall_bot/info_service.py
@@ -1,10 +1,18 @@
+""" Contains methods for assisting teh Info Cog. """
+
 from starfall_bot.definition_collector import DefinitionCollector
 
 
 class InfoService:
+    """ A class for dealing Info Cog requests """
     def __init__(self, definition_collector: DefinitionCollector):
         self._definition_collector = definition_collector
 
     def define(self, word: str):
+        """
+        Gets the definition for a given word.
+        :param word: The word to get a definition for.
+        :return: The definition of the word.
+        """
         definition = self._definition_collector.get_definition(word)
-        return f"{word}: {definition}"
+        return definition
