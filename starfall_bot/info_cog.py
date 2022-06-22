d2136c92ed17601c0352a4fa772f5935cea0c811
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-06-22 22:28:18 +0100

    Initial InfoCog commit.

---
+++ starfall_bot/info_cog.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from discord.ext.commands import Cog, Context
+
+from starfall_bot.info_service import InfoService
+
+
+class InfoCog(Cog):
+    def __init__(self, info_service: InfoService):
+        self._info_service = info_service
+
+    @commands.command()
+    def define(self, ctx: Context, *, word: str):
+        definition = self._info_service.define(word)
+
+        await ctx.send(f"> {word}: {definition}")
+
+    
